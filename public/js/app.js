1905f8292127e6e73683c76f2926b930f124eda4
vmayank-6 <vmayank624@gmail.com>
2020-10-14 21:51:10 +0530

    ii

--- public/js/app.js
+++ public/js/app.js
@@ -37320,7 +37320,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\ACER\\VoiceEnabledFormFilling\\resources\\sass\\app.scss'\n    at C:\\Users\\ACER\\VoiceEnabledFormFilling\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\ACER\\VoiceEnabledFormFilling\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\ACER\\VoiceEnabledFormFilling\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\Users\\ACER\\VoiceEnabledFormFilling\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
 
 /***/ }),
 
